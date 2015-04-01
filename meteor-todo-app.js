b35937da065c115b01b1874f000d3513c456222d
Andreas Bissinger <mail@bissinger-andreas.de>
2015-04-01 20:15:47 +0200

    Create templates for tabs and tab-riders

--- meteor-todo-app.js
+++ meteor-todo-app.js
@@ -1,4 +1,11 @@
 if (Meteor.isClient) {
+    Template.body.helpers({
+        tabs: [
+            { title: 'Done', opened: false },
+            { title: 'Open', opened: true },
+            { title: 'Closed', opened: false }
+        ]
+    });
 }
 
 if (Meteor.isServer) {
