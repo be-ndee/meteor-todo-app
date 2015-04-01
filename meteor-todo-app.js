a2d9fb2c18e81f093493a2e3d9d17de3cb26f75d
Andreas Bissinger <mail@bissinger-andreas.de>
2015-04-01 21:03:54 +0200

    Add click handler for tab, update style

--- meteor-todo-app.js
+++ meteor-todo-app.js
@@ -1,10 +1,28 @@
 if (Meteor.isClient) {
+    Session.set('tabs', [
+        { title: 'Done' },
+        { title: 'Open' },
+        { title: 'Closed' }
+    ]);
+    Session.set('selectedTab', 'Open');
+
+    // Body helpers
     Template.body.helpers({
-        tabs: [
-            { title: 'Done', opened: false },
-            { title: 'Open', opened: true },
-            { title: 'Closed', opened: false }
-        ]
+        tabs: function () {
+            return Session.get('tabs');
+        }
+    });
+
+    // Handler to check if tab is active
+    Handlebars.registerHelper('isActive', function (title) {
+        return Session.equals('selectedTab', title);
+    });
+
+    // Events on tabRider
+    Template.tabRider.events({
+        'click': function () {
+            Session.set('selectedTab', this.title);
+        }
     });
 }
 
